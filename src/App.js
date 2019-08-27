729fdc23ea82ae2cc30ce82a54f9e90d344a800f
Youval Polacsek <Youval@h-MacBook-Pro-sl-Youval.local>
2019-08-27 18:03:32 +0300

    i hope this works

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 // import Register from './components/Register';
 // import EmergencyContacts from './components/EmergencyContacts';
 import Sos from './components/Sos/Sos';
-// import UserProfile from './components/UserProfile'
-import EmergencyContacts from './components/UserProfile/EmergencyContacts'
+import UserProfile from './components/UserProfile/UserProfile'
+import Emergency from './components/Emergency/Emergency'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Register from './components/Register';
 
 
 class App extends Component{
@@ -16,12 +18,21 @@ class App extends Component{
 
   render(){
     return (
+      <Router>
       <div>
-        {/* <Register /> */}
-        <Sos />
-        {/* <Emergency /> */}
-        {/* <UserProfile /> */}
+      <Link to="/sos"><button className="links">SoS</button></Link>
+      <Link to="/userProfile"><button className="links">userProfile</button></Link>
+      <Link to="/register"><button className="links">register</button></Link>
+      <Link to="/emergency"><button className="links">emergency</button></Link>
+
+
+      <Route path="/register" exact render={() => <Register />} />
+      <Route path="/sos" exact render={() => <Sos />} />
+      <Route path="/userProfile" exact render={() => <UserProfile />} />
+      <Route path="/emergency" exact render={() => <Emergency />} />
+
       </div>
+      </Router>
     )
   }
 }

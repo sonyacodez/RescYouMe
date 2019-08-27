a82a907f9ddac1e3a3bf8d261483c0d29fe1f34b
Sonya Burlan <sonya.burlan@gmail.com>
2019-08-27 13:59:55 +0300

    Created UserStore and EmergencyContactStore

--- src/components/UserProfile/EmergencyContacts.js
+++ src/components/UserProfile/EmergencyContacts.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react'
+import PhoneNumber from './PhoneNumber'
 
 class EmergencyContacts extends Component {
+    constructor(){
+        super()
+        this.state = {
+            name: "",
+            phoneNumber: 0
+        }
+    }
     render() {
         return (
             <div>
-                {/* <PhoneNumber> */}
+                <div id="userPhoneNumbers">
+                    <PhoneNumber />
+                </div>
+                <div id="addForm">
+                    <div>Name: 
+                        <input name="firstName" value={this.state.name} onChange={this.updateClientKeys} type="text"/>
+                    </div>
+                    <div>Phone Number: 
+                        <input name="owner" value={this.state.phoneNumber} onChange={this.updateClientKeys} type="text"/>
+                    </div>
+                    <button onClick={this.addNewContact}>Add New Client</button>
+                </div>
             </div>
         )
     }
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import EmergencyContacts from './EmergencyContacts'
 
 class UserProfile extends Component {
     render() {
         return (
             <div>
-        
+                <EmergencyContacts />
             </div>
         )
     }

3019437172196fc462b0082949d50fbfe1287b8a
Youval Polacsek <Youval@h-MacBook-Pro-sl-Youval.local>
2019-08-27 11:48:25 +0300

    first map render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Register from './components/Register';
+import EmergencyContacts from './components/EmergencyContacts';
+import Sos from './components/Sos/Sos';
+import UserProfile from './components/UserProfile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Register />
+        <EmergencyContacts />
+        <Sos />
+        {/* <UserProfile /> */}
+      </div>
+    )
+  }
 }
 
 export default App;

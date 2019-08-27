54bdf69e4dc22d8ca2b7da0db6b62b674f16d02a
Sonya Burlan <sonya.burlan@gmail.com>
2019-08-27 15:12:45 +0300

    updated UserStore and added routes to apiClient

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,22 +1,15 @@
 /* eslint-disable no-unused-expressions */
-import { observable, action, computed } from "mobx";
-import EmergencyContactStore from "./EmergencyContact"
+import { observable, computed } from "mobx";
+import apiClient from "../apiClient"
 
 class UserStore {
-    @observable contactList = []
+    @observable userLocation = {
+        latitude: 0,
+        longitude: 0
+    }
+    @observable contactList = apiClient.getAllContacts()
 
     @computed get numContacts() { return this.contactList.length };
-
-    @action addContact = (name, phoneNumber) => {
-        this.contactList.push( new EmergencyContactStore(name, phoneNumber) )
-        // apiClient.methodtoadduser(name, phoneNumber)
-    }
-    @action changeContactNumber = (name, phoneNumber) => this.contactList.find( c => c.name === name ).phoneNumber = phoneNumber;
-
-    @action deleteContact = (name) => {
-        const index = this.contactList.findIndex(c => c.name === name)
-        this.contactList.splice(index, 1)
-    };
 }
 
 export default new UserStore()

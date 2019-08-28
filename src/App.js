58bd555d62b062d667457037e74e0f3cb45502ef
Katya <lopukhina.ekaterina@gmail.com>
2019-08-28 09:18:47 +0300

    added styling for navbar

--- src/App.js
+++ src/App.js
@@ -3,38 +3,52 @@ import Register from './components/Register/SignUp.js';
 // import EmergencyContacts from './components/UserProfile';
 import Sos from './components/Sos/Sos';
 import UserProfile from './components/UserProfile/UserProfile'
-import Emergency from './components/Emergency/Emergency'
+import Emergency from './components/Emergency/Help'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 // import Register from './components/Register';
-import Help from './components/Emergency/Help'
 
-class App extends Component{
-  constructor(){
+//Material UI
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: '#EAC563',
+      }
+    }
+  }
+);
+
+class App extends Component {
+  constructor() {
     super()
-    this.state={
+    this.state = {
 
     }
   }
 
-  render(){
+  render() {
     return (
       <Router>
-      <div>
-      <div style={{marginLeft:'14%'}}>
-      <Link to="/sos"><button className="links">SoS</button></Link>
-      <Link to="/userProfile"><button className="links">userProfile</button></Link>
-      <Link to="/register"><button className="links">register</button></Link>
-      <Link to="/emergency"><button className="links">emergency</button></Link>
-      <Link to="/help"><button className="links">help</button></Link>
-
-      </div>
-      <hr></hr>
-      <Route path="/register" exact render={() => <Register />} />
-      <Route path="/sos" exact render={() => <Sos />} />
-      <Route path="/userProfile" exact render={() => <UserProfile />} />
-      <Route path="/emergency" exact render={() => <Emergency />} />
-      <Route path="/help" exact render={() => <Help />} />
-      </div>
+        <MuiThemeProvider theme={theme}>
+          <AppBar position="static">
+    
+              <div style={{ marginLeft: '10%' }}>
+                <Button><Link className="links" to="/sos">SoS</Link></Button>
+                <Button><Link className="links" to="/userProfile">userProfile</Link></Button>
+                <Button><Link className="links" to="/register">register</Link></Button>
+                <Button><Link className="links" to="/emergency">emergency</Link></Button>
+              </div>
+      </AppBar>
+        </MuiThemeProvider>
+          <hr></hr>
+          <Route path="/register" exact render={() => <Register />} />
+          <Route path="/sos" exact render={() => <Sos />} />
+          <Route path="/userProfile" exact render={() => <UserProfile />} />
+          <Route path="/emergency" exact render={() => <Emergency />} />      
       </Router>
     )
   }

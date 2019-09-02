de9833d9de7be09a0d2c16af51cedaf5553eb966
Sonya Burlan <sonya.burlan@gmail.com>
2019-09-02 17:33:32 +0300

    ran build

---
+++ build/precache-manifest.17dfbe0e087e044809bf0c3465715172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f337cb5446501426b6449aa01ef9a554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be30f81203570c3086c",
+    "url": "/static/css/main.d4cda541.chunk.css"
+  },
+  {
+    "revision": "feab85ab1af187fc97b9",
+    "url": "/static/js/2.37286407.chunk.js"
+  },
+  {
+    "revision": "8be30f81203570c3086c",
+    "url": "/static/js/main.2f8123b7.chunk.js"
+  },
+  {
+    "revision": "6e9bc6c820435e865808",
+    "url": "/static/js/runtime~main.ee8b2f64.js"
+  }
+]);
---
+++ build/worker.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-restricted-globals */
+console.log('Loaded service worker!');
+
+self.addEventListener('push', event => {
+    const data = event.data.json()
+    console.log('New notification', data)
+    const options = {
+      body: data.body,
+    }
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+})

7dd422ca4aa499105380cc9616129defedf47efb
Sonya Burlan <sonya.burlan@gmail.com>
2019-09-02 19:31:41 +0300

    created updateUser route and added endpoint and subscriptionObject to user everytime they login or signup

--- src/apiClient.js
+++ src/apiClient.js
@@ -14,9 +14,11 @@ class ApiClient {
         return await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.key}`)
     };
 
-    findUser = async(name, email) => await axios.post(`${this.URLname}/existingUser`, { name, email });
+    findUser = async(name, email) => await axios.get(`${this.URLname}/existingUser`, { name, email });
 
-    addNewUser = async(name, email) => await axios.post(`${this.URLname}/user`, { name, email });
+    addNewUser = async(name, email, subscriptionObject) => await axios.post(`${this.URLname}/subscribe`, { name, email, subscriptionObject });
+    
+    updateUser = async(subscriptionObject) => await axios.put(`${this.URLname}/updateUser/${UserStore.currentUserID}`, { subscriptionObject });
 
     getAllContacts = async() => await axios.get(`${this.URLname}/userContacts/${UserStore.currentUserID}`);
 

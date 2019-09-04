c1e65feb6fb8db55f2f269eb302fea64a1c04886
Katya <lopukhina.ekaterina@gmail.com>
2019-09-04 22:27:56 +0300

    added actions to buttons

--- public/worker.js
+++ public/worker.js
@@ -2,15 +2,41 @@
 console.log('Loaded service worker!');
 
 self.addEventListener('push', event => {
-    const data = event.data.json()
-    const options = {
-      body: data.body
-    }
-    event.waitUntil(
-      self.registration.showNotification(data.title, options)
-    );
+  const data = event.data.json()
+  const options = {
+    body: data.body,
+    actions: [
+      {
+        action: 'show',
+        title: 'Show it',
+        icon: '/check.png'
+      },
+      {
+        action: 'ignore',
+        title: 'Ignore it',
+        icon: '/delete.png'
+      }
+    ]
+  }
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 })
 
-self.addEventListener('notificationclick', () => {
-  console.log('Clicked!');
+self.addEventListener('notificationclick', (event) => {
+  if (!event.action) {
+    console.log('No button clicked');
+    return;
+  }
+  switch (event.action) {
+    case 'show':
+      console.log('User wants to see more');
+      break;
+    case 'ignore':
+      console.log('User wants to ignore the notification');
+      break;
+    default:
+      console.log(`The ${event.action} action is unknown`);
+      break;
+  }
 });

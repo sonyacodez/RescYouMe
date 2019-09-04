7dbe95ff3c1272fe7c915689e9e92bd08254b806
Sonya Burlan <sonya.burlan@gmail.com>
2019-09-04 16:07:14 +0300

    change build script to use build.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/', api)
 
 // serve React app from client/build
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build/index.html'))
+  res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
 app.listen(port, function(){

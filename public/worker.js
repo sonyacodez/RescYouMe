f6add0691e1a7770d53c8d37cb3e4961420b1b4f
Katya <lopukhina.ekaterina@gmail.com>
2019-09-04 21:55:17 +0300

    deleted comma in worker

--- public/worker.js
+++ public/worker.js
@@ -3,7 +3,7 @@ console.log('Loaded service worker!');
 
 self.addEventListener('push', event => {
     const data = event.data.json()
-    const body = data.body,
+    const body = data.body
     const actions = [{
       action: "Accept", title: 'Accept'
     }]
